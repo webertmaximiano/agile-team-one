a94c08274308d367d9cdfb815b6fea0f31537087
Webert Maximiano <webertcoach@gmail.com>
2024-02-13 19:40:46 -0300

    add Flowbite

--- estouon-app/resources/js/Components/ApplicationLogo.vue
+++ estouon-app/resources/js/Components/ApplicationLogo.vue
@@ -2,11 +2,9 @@
 import { usePage } from '@inertiajs/vue3';
 
 const imageUrls = usePage().props.imageUrls;
-console.log('url da logo', imageUrls.logo)
+//console.log('url da logo', imageUrls.logo)
 
 </script>
 <template>
-    <div class="mt-2 mb-2">
         <img :src="imageUrls.logo" alt="" class="img-fluid">
-    </div>
 </template>
---
+++ estouon-app/resources/js/Layouts/SiteLayout.vue
@@ -0,0 +1,21 @@
+<script setup>
+ 
+
+
+</script>
+
+
+<template>
+    <div class="site-layout">
+                <!-- Page Content -->
+            <main>
+                <slot />
+            </main>
+    </div>
+</template>
+  
+<!-- Estilos -->    
+<style>
+
+</style>
+  
